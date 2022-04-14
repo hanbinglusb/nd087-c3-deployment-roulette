01b668b2993e573d91d9292e251ee2be25f926ae
Hanbing Li <hanbingl@gmail.com>
2022-04-14 05:23:14 -0400

    submit

--- starter/infra/kubernetes_resources.tf
+++ starter/infra/kubernetes_resources.tf
@@ -7,9 +7,36 @@ resource "kubernetes_namespace" "udacity" {
   ]
 }
 
-resource "kubernetes_service" "blue" {
+# resource "kubernetes_service" "blue" {
+#   metadata {
+#     name      = "blue-svc"
+#     namespace = local.name
+#     annotations = {
+#       "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
+#       "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
+#     }
+#   }
+#   spec {
+#     selector = {
+#       app = "blue"
+#     }
+
+#     port {
+#       port        = 80
+#       target_port = 80
+#     }
+
+#     type = "LoadBalancer"
+#   }
+
+#   depends_on = [
+#     module.project_eks
+#   ]
+# }
+
+resource "kubernetes_service" "green" {
   metadata {
-    name      = "blue-svc"
+    name      = "green-svc"
     namespace = local.name
     annotations = {
       "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
@@ -18,7 +45,7 @@ resource "kubernetes_service" "blue" {
   }
   spec {
     selector = {
-      app = "blue"
+      app = "green"
     }
 
     port {
